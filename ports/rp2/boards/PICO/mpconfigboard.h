92e469a1acb3481141ae453f07e79bb1033c7c71
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-12-20 18:11:59 +0530

    Update mpconfigboard.h

--- ports/rp2/boards/PICO/mpconfigboard.h
+++ ports/rp2/boards/PICO/mpconfigboard.h
@@ -1,3 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+
+#define MICROPY_HW_USB_VID (0x239A)
+#define MICROPY_HW_USB_PID (0x80F2)
